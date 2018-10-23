322dd4f72aba58bdf0d3fd6e2181d0776ed36e06
Isaac2357 <isaaccabrera31@gmail.com>
2018-10-23 16:51:17 -0500

    Issues Dashboard y NewProject.

--- app/src/main/java/com/wired/ctapp/consultall/LoginActivity.java
+++ app/src/main/java/com/wired/ctapp/consultall/LoginActivity.java
@@ -3,6 +3,7 @@ package com.wired.ctapp.consultall;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -40,7 +41,9 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
-     * Trying push from local repository.
+     * Trying push
+     *
+     * from local repository.
      */
 
     /**
@@ -191,6 +194,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intent = new Intent(this, DashboardActivity.class);
+            startActivity(intent);
+
         }
     }
 
